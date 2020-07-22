1c8cbde121ecabdf27614bc982920a50ecf348ac
Oliveia-86 <egberto.duarte.14@gmail.com>
2020-07-22 18:58:09 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,19 @@
 package application;
 
-import java.sql.Connection;
+import java.util.Date;
 
-import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "bob");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
 	}
 
 }
